21d8709336a83b0806f9b716b9eae51af9ddc2af
Astra-001 <60982780+Astra-001@users.noreply.github.com>
2020-09-18 23:17:01 +0530

    Add files via upload

---
+++ javaScript/navBar.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+    $("#bars").click(function(){
+        $("#navlink >ul").toggle();
+      });
+    //   $(".card-img-top , .one").mouseenter(function(){
+    //      $(".cardhover").css("display","flex");
+    //   });
+    //   $(".card-img-top , .one").mouseleave(function(){
+    //     $(".card1>.cardhover").css("display","none");
+    //  });
+    
+  
+  });
